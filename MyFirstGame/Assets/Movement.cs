5b8ace1e9714a1ffec6cc92b483ec08a2bdfbd08
susccrua <susccrua@gmail.com>
2020-04-16 15:55:22 -0500

    First script and assets
    Fix the gitignore. Updated movement script, camera background, imported racket, etc.

---
+++ MyFirstGame/Assets/Movement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    private KeyCode fwdKey, backKey, leftKey, rightKey;
+    private float avgSpeed;
+    public Rigidbody player;
+
+    Movement()
+    {
+        fwdKey = (KeyCode)'w';
+        backKey = (KeyCode)'s';
+        leftKey = (KeyCode)'a';
+        rightKey = (KeyCode)'d';
+        avgSpeed = 1200f;
+
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(fwdKey))
+            player.AddForce(0, 0, Time.deltaTime * avgSpeed);
+        if (Input.GetKey(backKey))
+            player.AddForce(0, 0, -(Time.deltaTime * avgSpeed));
+        if (Input.GetKey(rightKey))
+        player.AddForce(Time.deltaTime * avgSpeed, 0, 0);
+        if (Input.GetKey(leftKey))
+            player.AddForce(-(Time.deltaTime * avgSpeed), 0 , 0);
+    }
+}
+
+
+/*
+//UPDATE CONTROLLERS
+
+public void updateController{}
+     */
